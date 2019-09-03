20395df400c31d56e1fefa44ede8c0aad1e49594
Justin Miller <jayman1294@gmail.com>
2019-09-03 01:45:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,23 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {},
+     :capulet => {}
+   }
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+   }
 end
