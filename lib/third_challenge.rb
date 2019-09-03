1c6303dbb9e8b3e01f79e6c24db7c043021f0a95
Justin Miller <jayman1294@gmail.com>
2019-09-03 02:37:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,18 +17,7 @@ def third_challenge
         :status => "alive"
         #your key/value pairs here
         },
-      :hero_friends => [
-        {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        },
-        {
-          :name => "Mercutio",
-          :age =>"18",
-          :attitude => "hot-headed"
-        }
-      ]
+      :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
